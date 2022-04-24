263d3edcc4a705ec579f1d3e74f21e19816f7965
Ben Hailu <b.hailu.pk@gmail.com>
2022-04-24 16:48:13 -0400

    created new function and commented out old one - created computerPlay2 - commented out line 43-55

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-console.log("The mask");
+console.log("ROCK PAPER SCISSORS!");
 
 // function computerPlay(max) {
 //     return Math.floor(Math.random() * max);
@@ -10,19 +10,45 @@ console.log("The mask");
 //i will assign a string (rock paper or scissors) to each num
 
 
-function computerPlay(max) {
-    let randomNumber = Math.floor(Math.random()*max);
-    console.log(randomNumber); 
+// function computerPlay(max) {
+//     const randomNumber = Math.floor(Math.random()*max);
+//     // console.log(randomNumber); 
 
-    if (randomNumber == 0) {
-        console.log("Rock");
-    }
-    else if (randomNumber == 1){
-        console.log("Paper");
-    }
-    else if (randomNumber == 2) {
-        console.log("Scissors");
-    }
+//     if (randomNumber == 0) {
+//         console.log("CPU: Rock");
+//     }
+//     else if (randomNumber == 1){
+//         console.log("CPU: Paper");
+//     }
+//     else if (randomNumber == 2) {
+//         console.log("CPU: Scissors");
+//     }
+// }
+
+//testing a new way from stackoverflow
+
+function computerPlay2() {
+    let values = ["Rock", "Paper", "Scissors",],
+        valuesToUse = values[Math.floor(Math.random() * values.length)];
+        console.log("\n"+valuesToUse);
 }
+computerPlay2();
 
-computerPlay(3)
+
+
+
+
+// function playRound(playerSelection, computerSelection) {
+//     computerPlay(3)
+// }
+
+// const playerSelection = "rock";
+// const computerSelection = computerPlay(3);
+// console.log(playRound(playerSelection, computerSelection));
+
+// function game() {    
+//     for (let i = 0; i < 5; i++) {
+//         playRound()
+//     }
+// }
+
